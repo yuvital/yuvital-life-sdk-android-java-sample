2a400b4acb5d10e1ab920fa7dbe676db1c3b6565
Naum Asafov <nauma@yuvital.com>
2025-11-19 17:09:19 +0200

    initialize

---
+++ app/src/androidTest/java/yuvital/life/sdk/android/java/sample/ExampleInstrumentedTest.java
@@ -0,0 +1,23 @@
+package yuvital.life.sdk.android.java.sample;
+
+import android.content.Context;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+
+    @Test
+    public void useAppContext() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("yuvital.life.sdk.android.java.sample", appContext.getPackageName());
+    }
+}
+
+
---
+++ app/src/test/java/yuvital/life/sdk/android/java/sample/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package yuvital.life.sdk.android.java.sample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleUnitTest {
+
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
+
+
